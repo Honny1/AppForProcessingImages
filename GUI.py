dbd765cad8a2ddc1875ada7b3434c34222eeabec
Hony <hony.com@seznam.cz>
2018-05-11 14:24:45 +0200

    added chacking  user input and small change desing

--- GUI.py
+++ GUI.py
@@ -1,36 +1,40 @@
 from tkinter import *
 import Start
+import time
 
 
 def inicializaceOkna():
     okno.title("App")
-    okno.geometry("325x200+400+100")
+    okno.geometry("350x200+400+100")
 
 
 def obsahOkna():
-    L1 = Label(okno, text="SD Cart dir:")
+    L1 = Label(okno, text="Source dir:")
     L1.grid(row=0, column=0)
 
     global E1, E2, ch1, ch2, ch3, ch4
     E1 = Entry(okno, textvariable=src, width=20)
     E1.grid(row=0, column=1)
 
-    L2 = Label(okno, text="New dir:")
+    L2 = Label(okno, text="   New dir:")
     L2.grid(row=2, column=0)
 
     E2 = Entry(okno, textvariable=Dir, width=20)
     E2.grid(row=2, column=1)
 
-    B1 = Button(okno, text="RUN", bg="yellow", command=run)
+    B1 = Button(okno, text="         RUN         ", bg="yellow", command=run)
     B1.grid(row=3, column=0)
+
+    B2 = Button(okno, text="PUSH ON WEB", bg="RED", command=pushOnWeb)
+    B2.grid(row=4, column=0)
 
     Ch1 = Checkbutton(okno, text="  Copy                               ", variable=ch1)
     Ch1.grid(row=0, column=2)
 
-    Ch2 = Checkbutton(okno, text="  Sort  (Jpg and Raw)       ", variable=ch2)
+    Ch2 = Checkbutton(okno, text="  Sort  (Jpg and Nef)        ", variable=ch2)
     Ch2.grid(row=2, column=2)
 
-    Ch3 = Checkbutton(okno, text="  Fix (Only SD Cart)          ", variable=ch3)
+    Ch3 = Checkbutton(okno, text="  Fix Images                      ", variable=ch3)
     Ch3.grid(row=3, column=2)
 
     Ch4 = Checkbutton(okno, text="  Rename RAW                 ", variable=ch4)
@@ -39,13 +43,11 @@ def obsahOkna():
     Ch5 = Checkbutton(okno, text="  Rename JPG                   ", variable=ch5)
     Ch5.grid(row=5, column=2)
 
-
-
-
 def zviditelneniOkna():
     okno.mainloop()
 
-
+def pushOnWeb():
+    print "Preparing..."
 def run():
     copy = ch1.get()
     sort = ch2.get()
@@ -56,13 +58,17 @@ def run():
     newDir = Dir.get()
     sourceDir = src.get()
 
+    L3 = Label(okno, text="  Processing...      ")
+    L3.grid(row=3, column=1)
+
+    okno.update()
 
     print(copy, sort, fixImage, renameRaw, renameJpg, newDir, sourceDir)
     done=Start.start(copy, sort, fixImage, renameRaw, renameJpg, newDir, sourceDir)
+    print(done)
 
-    x = "PROCES DONE!!!"
-    L3 = Label(okno, text=x)
-    L3.grid(row=4, column=1)
+    L3 = Label(okno, text=done)
+    L3.grid(row=3, column=1)
 
 
 
@@ -76,9 +82,6 @@ ch4 = IntVar()
 ch5 = IntVar()
 src = StringVar()
 Dir = StringVar()
-
-global done
-done=0
 
 inicializaceOkna()
 obsahOkna()
--- Start.py
+++ Start.py
@@ -2,50 +2,158 @@ import CopyFilesFromSDCart
 import ImageProcessing
 import RenameImages
 import SortFilesToDirectory
+import os
 
 def start(copy, sort, fixImage, renameRaw, renameJpg,  newDirectory, sourceDirectory):
 
-    nefDirectory=newDirectory+"/Nef"
-    jpgDirectory=newDirectory+"/Jpg"
-    if copy == 0 and sort == 0 and renameJpg == 0 and renameRaw == 0 and fixImage == 0:
-        return "NothingIsSelected"
-    elif copy == 0 and sort == 0 and renameJpg == 0 and renameRaw == 0 and fixImage == 1:
-        ImageProcessing.fixImage(sourceDirectory)
-    elif copy == 0 and sort == 0 and renameJpg == 0 and renameRaw == 1 and fixImage == 0:
-    elif copy == 0 and sort == 0 and renameJpg == 0 and renameRaw == 1 and fixImage == 1:
-    elif copy == 0 and sort == 0 and renameJpg == 1 and renameRaw == 0 and fixImage == 0:
-    elif copy == 0 and sort == 0 and renameJpg == 1 and renameRaw == 0 and fixImage == 1:
-    elif copy == 0 and sort == 0 and renameJpg == 1 and renameRaw == 1 and fixImage == 0:
-    elif copy == 0 and sort == 0 and renameJpg == 1 and renameRaw == 1 and fixImage == 1:
-    elif copy == 0 and sort == 1 and renameJpg == 0 and renameRaw == 0 and fixImage == 0:
-    elif copy == 0 and sort == 1 and renameJpg == 0 and renameRaw == 0 and fixImage == 1:
-    elif copy == 0 and sort == 1 and renameJpg == 0 and renameRaw == 1 and fixImage == 0:
-    elif copy == 0 and sort == 1 and renameJpg == 0 and renameRaw == 1 and fixImage == 1:
-    elif copy == 0 and sort == 1 and renameJpg == 1 and renameRaw == 0 and fixImage == 0:
-    elif copy == 0 and sort == 1 and renameJpg == 1 and renameRaw == 0 and fixImage == 1:
-    elif copy == 0 and sort == 1 and renameJpg == 1 and renameRaw == 1 and fixImage == 0:
-    elif copy == 0 and sort == 1 and renameJpg == 1 and renameRaw == 1 and fixImage == 1:
-    elif copy == 1 and sort == 0 and renameJpg == 0 and renameRaw == 0 and fixImage == 0:
-    elif copy == 1 and sort == 0 and renameJpg == 0 and renameRaw == 0 and fixImage == 1:
-    elif copy == 1 and sort == 0 and renameJpg == 0 and renameRaw == 1 and fixImage == 0:
-    elif copy == 1 and sort == 0 and renameJpg == 0 and renameRaw == 1 and fixImage == 1:
-    elif copy == 1 and sort == 0 and renameJpg == 1 and renameRaw == 0 and fixImage == 0:
-    elif copy == 1 and sort == 0 and renameJpg == 1 and renameRaw == 0 and fixImage == 1:
-    elif copy == 1 and sort == 0 and renameJpg == 1 and renameRaw == 1 and fixImage == 0:
-    elif copy == 1 and sort == 0 and renameJpg == 1 and renameRaw == 1 and fixImage == 1:
-    elif copy == 1 and sort == 1 and renameJpg == 0 and renameRaw == 0 and fixImage == 0:
-    elif copy == 1 and sort == 1 and renameJpg == 0 and renameRaw == 0 and fixImage == 1:
-    elif copy == 1 and sort == 1 and renameJpg == 0 and renameRaw == 1 and fixImage == 0:
-    elif copy == 1 and sort == 1 and renameJpg == 0 and renameRaw == 1 and fixImage == 1:
-    elif copy == 1 and sort == 1 and renameJpg == 1 and renameRaw == 0 and fixImage == 0:
-    elif copy == 1 and sort == 1 and renameJpg == 1 and renameRaw == 0 and fixImage == 1:
-    elif copy == 1 and sort == 1 and renameJpg == 1 and renameRaw == 1 and fixImage == 0:
-    elif copy == 1 and sort == 1 and renameJpg == 1 and renameRaw == 1 and fixImage == 1:
-
-        CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
-        SortFilesToDirectory.sortFilesToDirectory(newDirectory)
-        RenameImages.renameJpgImages(jpgDirectory)
-        RenameImages.renameNefImages(nefDirectory)
-        ImageProcessing.fixImage(jpgDirectory)
-
-    return 1
+    nefDirectory = newDirectory+"/Nef"
+    jpgDirectory = newDirectory+"/Jpg"
+    if sourceDirectory == "" or False == os.path.exists(sourceDirectory):
+        return "NotFoundSourceDir"
+    elif copy == 1 and newDirectory == "":
+        return "MissingNewDir"
+    else:
+        if copy == 0 and sort == 0 and renameJpg == 0 and renameRaw == 0 and fixImage == 0:
+         return "  NothingIsSelected  "
+        elif copy == 0 and sort == 0 and renameJpg == 0 and renameRaw == 0 and fixImage == 1:
+            ImageProcessing.fixImage(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 0 and renameJpg == 0 and renameRaw == 1 and fixImage == 0:
+            RenameImages.renameNefImages(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 0 and renameJpg == 0 and renameRaw == 1 and fixImage == 1:
+            RenameImages.renameNefImages(sourceDirectory)
+            ImageProcessing.fixImage(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 0 and renameJpg == 1 and renameRaw == 0 and fixImage == 0:
+            RenameImages.renameJpgImages(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 0 and renameJpg == 1 and renameRaw == 0 and fixImage == 1:
+            RenameImages.renameJpgImages(sourceDirectory)
+            ImageProcessing.fixImage(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 0 and renameJpg == 1 and renameRaw == 1 and fixImage == 0:
+            RenameImages.renameJpgImages(sourceDirectory)
+            RenameImages.renameNefImages(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 0 and renameJpg == 1 and renameRaw == 1 and fixImage == 1:
+            RenameImages.renameJpgImages(sourceDirectory)
+            RenameImages.renameNefImages(sourceDirectory)
+            ImageProcessing.fixImage(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 1 and renameJpg == 0 and renameRaw == 0 and fixImage == 0:
+            SortFilesToDirectory.sortFilesToDirectory(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 1 and renameJpg == 0 and renameRaw == 0 and fixImage == 1:
+            SortFilesToDirectory.sortFilesToDirectory(sourceDirectory)
+            ImageProcessing.fixImage(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 1 and renameJpg == 0 and renameRaw == 1 and fixImage == 0:
+            SortFilesToDirectory.sortFilesToDirectory(sourceDirectory)
+            RenameImages.renameNefImages(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 1 and renameJpg == 0 and renameRaw == 1 and fixImage == 1:
+            SortFilesToDirectory.sortFilesToDirectory(sourceDirectory)
+            RenameImages.renameNefImages(sourceDirectory)
+            ImageProcessing.fixImage(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 1 and renameJpg == 1 and renameRaw == 0 and fixImage == 0:
+            SortFilesToDirectory.sortFilesToDirectory(sourceDirectory)
+            RenameImages.renameJpgImages(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 1 and renameJpg == 1 and renameRaw == 0 and fixImage == 1:
+            RenameImages.renameJpgImages(sourceDirectory)
+            ImageProcessing.fixImage(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 1 and renameJpg == 1 and renameRaw == 1 and fixImage == 0:
+            SortFilesToDirectory.sortFilesToDirectory(sourceDirectory)
+            RenameImages.renameJpgImages(sourceDirectory)
+            RenameImages.renameNefImages(sourceDirectory)
+            return "OK"
+        elif copy == 0 and sort == 1 and renameJpg == 1 and renameRaw == 1 and fixImage == 1:
+            SortFilesToDirectory.sortFilesToDirectory(sourceDirectory)
+            RenameImages.renameJpgImages(sourceDirectory)
+            RenameImages.renameNefImages(sourceDirectory)
+            ImageProcessing.fixImage(sourceDirectory)
+            return "OK"
+        elif copy == 1 and sort == 0 and renameJpg == 0 and renameRaw == 0 and fixImage == 0:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 0 and renameJpg == 0 and renameRaw == 0 and fixImage == 1:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            ImageProcessing.fixImage(newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 0 and renameJpg == 0 and renameRaw == 1 and fixImage == 0:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            RenameImages.renameNefImages(newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 0 and renameJpg == 0 and renameRaw == 1 and fixImage == 1:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            ImageProcessing.fixImage(newDirectory)
+            RenameImages.renameNefImages(newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 0 and renameJpg == 1 and renameRaw == 0 and fixImage == 0:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            RenameImages.renameJpgImages(newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 0 and renameJpg == 1 and renameRaw == 0 and fixImage == 1:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            ImageProcessing.fixImage(newDirectory)
+            RenameImages.renameJpgImages(newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 0 and renameJpg == 1 and renameRaw == 1 and fixImage == 0:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            RenameImages.renameJpgImages(newDirectory)
+            RenameImages.renameNefImages(newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 0 and renameJpg == 1 and renameRaw == 1 and fixImage == 1:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            ImageProcessing.fixImage(newDirectory)
+            RenameImages.renameJpgImages(newDirectory)
+            RenameImages.renameNefImages(newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 1 and renameJpg == 0 and renameRaw == 0 and fixImage == 0:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            SortFilesToDirectory.sortFilesToDirectory(newDirectory)
+            return "OK"
+        elif copy == 1 and sort == 1 and renameJpg == 0 and renameRaw == 0 and fixImage == 1:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            SortFilesToDirectory.sortFilesToDirectory(newDirectory)
+            ImageProcessing.fixImage(jpgDirectory)
+            return "OK"
+        elif copy == 1 and sort == 1 and renameJpg == 0 and renameRaw == 1 and fixImage == 0:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            SortFilesToDirectory.sortFilesToDirectory(newDirectory)
+            RenameImages.renameNefImages(nefDirectory)
+            return "OK"
+        elif copy == 1 and sort == 1 and renameJpg == 0 and renameRaw == 1 and fixImage == 1:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            SortFilesToDirectory.sortFilesToDirectory(newDirectory)
+            RenameImages.renameNefImages(nefDirectory)
+            ImageProcessing.fixImage(jpgDirectory)
+            return "OK"
+        elif copy == 1 and sort == 1 and renameJpg == 1 and renameRaw == 0 and fixImage == 0:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            SortFilesToDirectory.sortFilesToDirectory(newDirectory)
+            RenameImages.renameJpgImages(jpgDirectory)
+            return "OK"
+        elif copy == 1 and sort == 1 and renameJpg == 1 and renameRaw == 0 and fixImage == 1:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            SortFilesToDirectory.sortFilesToDirectory(newDirectory)
+            RenameImages.renameJpgImages(jpgDirectory)
+            ImageProcessing.fixImage(jpgDirectory)
+            return "OK"
+        elif copy == 1 and sort == 1 and renameJpg == 1 and renameRaw == 1 and fixImage == 0:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            SortFilesToDirectory.sortFilesToDirectory(newDirectory)
+            RenameImages.renameJpgImages(renameJpg)
+            RenameImages.renameNefImages(renameRaw)
+            return "OK"
+        elif copy == 1 and sort == 1 and renameJpg == 1 and renameRaw == 1 and fixImage == 1:
+            CopyFilesFromSDCart.copyFiles(sourceDirectory, newDirectory)
+            SortFilesToDirectory.sortFilesToDirectory(newDirectory)
+            RenameImages.renameJpgImages(jpgDirectory)
+            RenameImages.renameNefImages(nefDirectory)
+            ImageProcessing.fixImage(jpgDirectory)
+            return "OK"
+        return "ERROR"
