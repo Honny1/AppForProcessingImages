54b370110c516ce3da81e9b1d3ff1caaf7a6cc0f
Jan Rodák <rodak.jan@ukuknet.intra>
2018-09-11 14:16:39 +0200

    adding a whole folder to a new folder via ssh

--- TESTs/Futures/testSSH.py
+++ TESTs/Futures/testSSH.py
@@ -1,11 +1,40 @@
 import os
 import paramiko
+from tqdm import tqdm
+
+files = []
+def processData(localpath,repotepath):
+    fileslocalpath = []
+    filesremotepath = []
+    src_files = os.listdir(localpath)
+    for file in src_files:
+        fileslocalpath.append(os.path.join(localpath, file))
+    for file in src_files:
+        filesremotepath.append(os.path.join(repotepath, file))
+    files.append(fileslocalpath)
+    files.append(filesremotepath)
+    return files
+
 def ssh(localpath, remotepath, server, username, password):
+    processData(localpath, remotepath)
     ssh = paramiko.SSHClient()
-    ssh.load_host_keys(os.path.expanduser(os.path.join("~", ".ssh", "known_hosts")))
+    ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
     ssh.connect(server, username=username, password=password)
+
     sftp = ssh.open_sftp()
-    sftp.put(localpath, remotepath)
+    try:
+        sftp.chdir(remotepath)
+    except IOError:
+        sftp.mkdir(remotepath)
+        sftp.chdir(remotepath)
     sftp.close()
+
+    sftp = ssh.open_sftp()
+    for i in tqdm(range(len(files[0]))):
+        sftp.put(files[0][i], files[1][i])
+    sftp.close()
+
     ssh.close()
-ssh(input("Zadej cestu k datum: "), input("Zadej cestu kam uložit data: "), input("Zadej server:"), input("Zadej username: "), input("Zadej password: "))
+
+ssh("C:\\temp\hony\\100D3300", "C:\\Hony\\test2", "WPBRN201", "rodak.jan", "******")
+
